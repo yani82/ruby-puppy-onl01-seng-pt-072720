c91b6d3dfa281d3c0a8062b3314aa2944b483c6d
Yani Asari <yani.iman1@gmail.com>
2020-08-19 05:26:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
-# Add your code here
+#describe "Dog" do
+class Dog 
+  
+  @@all = []
+  
+  attr_accessor :name
+  
+  def initialize(name)
+        @name = name
+        self.save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all 
+    @@all = []
+    puts #{Array.clear_all}
+  end 
+  
+  def self.print_all 
+     @@all.each do |d|
+      puts d.name 
+      end 
+  end 
+  
+  def save
+    @@all << self
+  end
+
+  def name
+    @name
+  end
+  
+end    
+    
