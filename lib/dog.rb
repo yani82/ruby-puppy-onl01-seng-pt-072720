f2fca2446b2838178e5811cf414b3a6fb5d9e25b
Yani Asari <yani.iman1@gmail.com>
2020-08-19 03:23:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,3 @@
 #describe "Dog" do
-def Dog.new 
-  
-  def initialize(name)
-        @name = name
-  end
-  
-   def name 
-        @name
-        puts self
-    end
-    
-    
-  
-end    
-    
 
  
